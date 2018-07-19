0cd472a582cf7e0ce9f5df320dd7eb7cf2ab028f
Anna Shipilova <anyashi2002@gmail.com>
2018-07-19 19:38:32 +0300

    Close Main Activity method

--- app/src/main/java/com/rdc/android_test_app_b/MainActivity.java
+++ app/src/main/java/com/rdc/android_test_app_b/MainActivity.java
@@ -1,5 +1,8 @@
 package com.rdc.android_test_app_b;
 
+import android.content.DialogInterface;
+import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        closeActivity();
+        
     }
+
+    private void closeActivity() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("WARNING")
+                .setMessage("Sorry, but you can't call this app from launcher, Please, open first app for following work." +
+                        '\n' + "This app will be automatically close in 10 seconds.")
+                .setCancelable(false)
+                .setNegativeButton("BACK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                                finish();
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
 }
