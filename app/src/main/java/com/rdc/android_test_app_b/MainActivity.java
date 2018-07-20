cae1101da0a9add8bc3837c91217abde74da84a4
Valerii Pushkov <valeragamig@gmail.com>
2018-07-20 12:59:46 +0300

    Commit to refresh master

--- app/src/main/java/com/rdc/android_test_app_b/MainActivity.java
+++ app/src/main/java/com/rdc/android_test_app_b/MainActivity.java
@@ -4,10 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+String status="sdv";
+String st="ff";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    public class Check{
+
     }
 }
