b666b78c12bbc383a8e33d58ae9088cbc27bc796
Roman Tkachenko <web1maxim@gmail.com>
2018-07-21 20:13:04 +0300

    Applications A & B connected

--- app/src/main/java/com/rdc/android_test_app_b/MainActivity.java
+++ app/src/main/java/com/rdc/android_test_app_b/MainActivity.java
@@ -3,10 +3,9 @@ package com.rdc.android_test_app_b;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -17,26 +16,20 @@ import com.squareup.picasso.Picasso;
 public class MainActivity extends AppCompatActivity {
 
 
-    ImageView iV;
-    String url = "https://skat/img.=./attach/12000/12669.jpg";
-    int status =0;
-    static TextView tv;
-    String text = "";
+    String linkToImg = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        iV = findViewById(R.id.imageView);
-        tv = findViewById(R.id.tvid);
+        linkToImg = getIntent().getStringExtra("url_from_A");
 
-        text = getIntent().getStringExtra("param1");
         boolean check = getIntent().getBooleanExtra("bool", false);
 
         if (check) {
             Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-            intent.putExtra("url", text);
+            intent.putExtra("url", linkToImg);
             startActivity(intent);
         } else {
             closeActivity();
@@ -65,29 +58,5 @@ public class MainActivity extends AppCompatActivity {
                         });
         AlertDialog alert = builder.create();
         alert.show();
-        loadImgByUrl(url, iV, this);
-    }
-   public void loadImgByUrl(String url, ImageView imageView, final Context context) {
-        Picasso.with(context).load(url).placeholder(R.mipmap.ic_launcher)               //default image(error)
-                .error(R.mipmap.ic_launcher)
-                .into(imageView, new com.squareup.picasso.Callback() {
-
-                    @Override
-                    public void onSuccess() {
-                        Toast.makeText(context, "Loading success", Toast.LENGTH_LONG).show();
-                        status = 1;
-                        nextAct(status);
-                    }
-
-                    @Override
-                    public void onError() {
-                        Toast.makeText(context, "Loading error", Toast.LENGTH_LONG).show();
-                        status = 2;
-                        nextAct(status);
-                    }
-                });
-    }
-    public void nextAct(int status){
-
     }
 }
--- app/src/main/java/com/rdc/android_test_app_b/SecondActivity.java
+++ app/src/main/java/com/rdc/android_test_app_b/SecondActivity.java
@@ -1,13 +1,54 @@
 package com.rdc.android_test_app_b;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 public class SecondActivity extends AppCompatActivity {
+
+
+    ImageView out_image;
+    String url = "";
+    int status = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        out_image = findViewById(R.id.imageViewOut);
+
+        Intent get_link = getIntent();
+        url = get_link.getStringExtra("url");
+        loadImgByUrl(url, out_image, this);
+    }
+    public void loadImgByUrl(String url, ImageView imageView, final Context context) {
+        Picasso.with(context).load(url).placeholder(R.mipmap.ic_launcher)               //default image(error)
+                .error(R.mipmap.ic_launcher)
+                .into(imageView, new com.squareup.picasso.Callback() {
+
+                    @Override
+                    public void onSuccess() {
+                        Toast.makeText(context, "Loading success", Toast.LENGTH_LONG).show();
+                        status = 0;
+                        nextAct(status);
+                    }
+
+                    @Override
+                    public void onError() {
+                        Toast.makeText(context, "Loading error", Toast.LENGTH_LONG).show();
+                        status = 1;
+                        nextAct(status);
+                    }
+                });
+    }
+    public void nextAct(int status){
+
     }
 }
